a8e991b546dcf464a2403f4686f63cae4a25e575
lorniu <lorniu@gmail.com>
2019-08-16 06:41:55 +0000

    a little change to sly.lisp

--- scripts/lisp/sly.lisp
+++ scripts/lisp/sly.lisp
@@ -2,26 +2,115 @@
 
 
 (defparameter *vps* "imxx.top" "Private Host")
+
 (defparameter imlisp-home "~/.notes/x.code.lisp/")
 (defparameter imlisp-local "~/vvv/lisp-local/")
-(defparameter *ql-home*
-  #+ros.init (merge-pathnames "lisp/quicklisp/" (roswell.util:homedir))
-  "~/.quicklisp/")
+(defparameter *ql-home* #+ros.init (merge-pathnames "lisp/quicklisp/" (roswell.util:homedir)) "~/.quicklisp/")
 
-
+
 ;;; quicklisp
-#-quicklisp (load (merge-pathnames "setup.lisp" *ql-home*) :verbose t)
+
+#-quicklisp
+(load (merge-pathnames "setup.lisp" *ql-home*) :verbose t)
+
+(setf (fdefinition 'qload) #'ql:quickload)
+
 (pushnew imlisp-local ql:*local-project-directories*)
 
-;;; some hacks for cl package
-(load (merge-pathnames "util.lisp" *load-truename*))
-(load (merge-pathnames "plus.lisp" *load-truename*))
+
+;;; dexador
 
-;;; external settings if possible
+(ql:quickload :dexador)
+
+(in-package :dexador)
+
+(shadow 'trace) (export 'trace)
+
+(defun trace (uri &rest args
+              &key version headers basic-auth cookie-jar keep-alive use-connection-pool timeout max-redirects
+                force-binary want-stream
+                ssl-key-file ssl-cert-file ssl-key-password stream verbose proxy insecure ca-path)
+  (declare (ignore version headers basic-auth cookie-jar keep-alive use-connection-pool timeout max-redirects force-binary want-stream ssl-key-file ssl-cert-file ssl-key-password stream verbose proxy insecure ca-path))
+  (let ((*verbose* t))
+    (apply #'request uri :method :get args)
+    (values)))
+
+
+;;; for convenience
+
+(in-package :cl)
+
+(defmacro qload-and-in (pkg)
+  `(progn
+     (ql:quickload ,pkg)
+     (in-package ,pkg)))
+
+(defmacro defpackage-and-in (&optional (pkg :cl-user) &rest args)
+  (unless args
+    (setq args '((:use :cl))))
+  `(progn
+     (defpackage ,pkg ,@args)
+     (in-package ,pkg)))
+
+(defun dir-symbols (package &key filter all (fn #'print))
+  "Print symbols in certain package."
+  (let (lst)
+    (flet ((add-match-to-list (p)
+             (if (or (not filter)
+                     (if (functionp filter) (funcall filter p)
+                         (search (string-upcase filter) (symbol-name p))))
+                 (push p lst))))
+      (if all
+          (do-symbols (s (find-package package) lst)
+            (add-match-to-list s))
+          (do-external-symbols (s (find-package package) lst)
+            (add-match-to-list s))))
+    (mapc fn (sort lst #'string<))) t)
+
+(defmacro alias-package (package nickname)
+  `(let ((package ,package) (nickname ,nickname))
+     (when (or (stringp package) (keywordp package))
+       (setf package (find-package package)))
+     (check-type package package)
+     (check-type nickname (or string keyword))
+     (rename-package package (package-name package)
+                     (adjoin (symbol-name nickname) (package-nicknames package)
+                             :test #'string=))
+     (package-nicknames package)))
+
+(defun alias% (as symbol &key (package *package*))
+  (if (fboundp symbol) (setf (symbol-function as) (symbol-function symbol)))
+  (if (boundp symbol) (setf (symbol-value as) (symbol-value symbol)))
+  (setf (symbol-plist as) (symbol-plist symbol))
+  (shadowing-import as package))
+
+(defmacro import-as (symbol as &key (package *package*))
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (alias% ',as ',symbol :package ,package)))
+
+(defun load-relative (filename)
+  (load (compile-file (merge-pathnames filename *load-truename*))))
+
+
+;;; load settings from external file
+
+(in-package :cl-user)
+
 (load (merge-pathnames "bootstrap.lisp" imlisp-home) :if-does-not-exist nil)
 
-
+
 ;;; miscellaneous
-(export '(*vps*))
+
+(export '(*vps*
+          qload
+          qload-and-in
+          defpackage-and-in
+          dir-symbols
+          alias-package
+          import-as
+          load-relative
+          ))
+
 (pushnew :im-loaded *features*)
+
 (declaim (optimize (debug 3)))
