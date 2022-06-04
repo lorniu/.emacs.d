18700d501d9f3b535837b6bbba40003d77b9c820
lorniu <lorniu@gmail.com>
2022-06-04 12:10:03 +0800

    Vim emulator.

--- core/imod-evil.el
+++ core/imod-evil.el
@@ -1,6 +1,14 @@
 ;;; imod-evil.el --- Emulate VI -*- lexical-binding: t -*-
 
+;; Viper: Basic Vi emulation mode offered by Emacs, builtin.
+;; Evil:  A completely new Vim emulation mode for Emacs and is said to be the successor of both Viper and Vimpulse mode.
+
 ;;; Code:
+
+(x viper
+   :init
+   (setq viper-inhibit-startup-message t)
+   (setq viper-expert-level 5))
 
 (x evil
    :ref "emacs-evil/evil"
